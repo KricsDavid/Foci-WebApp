231c10f0c59576b434ad9062392e99bef8b1b842
KricsDavid <david@kricsfalusi.hu>
2024-09-16 08:54:38 +0200

    builder

--- Orai/Program.cs
+++ Orai/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Orai;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<FociDbContext>
+    (options => options.UseSqlite
+    (connectionString:"Data Source = Foci.db"));
 
 var app = builder.Build();
 
