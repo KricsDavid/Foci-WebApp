d567ae0a43df2e6810e02faa363bd8aa7e7d522f
KricsDavid <david@kricsfalusi.hu>
2024-09-16 08:51:24 +0200

    ostaly, kapcsolat

---
+++ Orai/FociDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Orai
+{
+    public class FociDbContext :DbContext
+    {
+
+        public FociDbContext(DbContextOptions<FociDbContext> options)
+            :base(options)        
+        {
+            
+        }
+
+        public DbSet<Meccs> Meccsek {  get; set; }
+    }
+}
---
+++ Orai/Meccs.cs
@@ -0,0 +1,15 @@
+﻿namespace Orai
+{
+    public class Meccs
+    {
+        public int Id { get; set; }
+        public int Fordulo { get; set; }
+        public int HazaiGol { get; set; }
+        public int VendegGol { get; set; }
+        public int HazaiFelidogol { get; set; }
+        public int VendegFelidogol { get; set; }
+        public string HazaiCsapat { get; set; }
+        public string VendegCsapat { get; set; }
+
+    }
+}
