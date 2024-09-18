98bc03bcccfc7db272a2a482c375758bd9ed149f
KricsDavid <david@kricsfalusi.hu>
2024-09-18 12:42:32 +0200

    rfrr

---
+++ Orai/Migrations/20240918084740_sqlite.local_migration_982.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Orai.Migrations
+{
+    /// <inheritdoc />
+    public partial class sqlitelocal_migration_982 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meccsek",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fordulo = table.Column<int>(type: "INTEGER", nullable: false),
+                    HazaiGol = table.Column<int>(type: "INTEGER", nullable: false),
+                    VendegGol = table.Column<int>(type: "INTEGER", nullable: false),
+                    HazaiFelidogol = table.Column<int>(type: "INTEGER", nullable: false),
+                    VendegFelidogol = table.Column<int>(type: "INTEGER", nullable: false),
+                    HazaiCsapat = table.Column<string>(type: "TEXT", nullable: false),
+                    VendegCsapat = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meccsek", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Meccsek");
+        }
+    }
+}
---
+++ Orai/Pages/UjjMeccs.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Orai;
+
+namespace Orai.Pages
+{
+    public class UjjMeccsModel : PageModel
+    {
+        private readonly Orai.FociDbContext _context;
+
+        public UjjMeccsModel(Orai.FociDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Meccs Meccs { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Meccsek.Add(Meccs);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
